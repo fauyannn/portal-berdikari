8a3888bb89a0afa4701d321d09e28eb8715a57c4
fauyannn <fauyannn@gmail.com>
2020-03-18 13:56:02 +0700

    fix response api delivery note

--- app/Http/Controllers/ApiDeliveryNoteController.php
+++ app/Http/Controllers/ApiDeliveryNoteController.php
@@ -30,6 +30,8 @@
 				//This method will be execute after run the main process
 				$id = $result['id'];
 
+				unset($result['api_response_fields']);
+				unset($result['qr_code']);
 				$items = \DB::table('delivery_note_items')
 				->select('item_code','item_name','qty','uom','rate','amount')
 				->where('delivery_note_id',$id)
